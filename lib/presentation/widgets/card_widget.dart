939b0d87cb30a8bc62ee9173c351310370ec6970
mohammedsajadvt <mohammedsajadvt@gmail.com>
2024-06-29 21:05:50 +0530

    changes

--- lib/presentation/widgets/card_widget.dart
+++ lib/presentation/widgets/card_widget.dart
@@ -11,19 +11,19 @@ class CardWidget extends StatelessWidget {
         Expanded(
           child: Card(
             child: Container(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  Image.asset('images/Super Supreme.png'),
-                  SizedBox(height: 8.0),
-                  Text('Super Supreme',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-                  Text('Daging Sapi Asap, Daging Ayam Asap, Burger Sapi, Jamur, Nanas, Paprika dan Keju Mozzarella',overflow: TextOverflow.ellipsis,),
-                  Text('₹199',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                  Image.asset('images/Super Supreme.png',),
+                  const SizedBox(height: 8.0),
+                  const Text('Super Supreme',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                  const Text('Daging Sapi Asap, Daging Ayam Asap, Burger Sapi, Jamur, Nanas, Paprika dan Keju Mozzarella',overflow: TextOverflow.ellipsis,),
+                  const Text('₹199',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
                    Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                    IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.cart)),
-                    IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.heart))
+                    IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.cart)),
+                    IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.heart))
                   ],)
                 ],
               ),
@@ -33,19 +33,19 @@ class CardWidget extends StatelessWidget {
         Expanded(
           child: Card(
             child: Container(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  Image.asset('images/pizza.jpg'),
-                  SizedBox(height: 8.0),
-                  Text('Frankfurter BBQ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-                  Text('Daging Sapi Asap, Daging Ayam Asap, Burger Sapi, Jamur, Nanas, Paprika dan Keju Mozzarella',overflow: TextOverflow.ellipsis,),
-                  Text('₹199',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                  Image.asset('images/Frankfurter BBQ.png'),
+                  const SizedBox(height: 8.0),
+                  const Text('Frankfurter BBQ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                  const Text('Daging Sapi Asap, Daging Ayam Asap, Burger Sapi, Jamur, Nanas, Paprika dan Keju Mozzarella',overflow: TextOverflow.ellipsis,),
+                  const Text('₹199',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                    IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.cart)),
-                    IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.heart))
+                    IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.cart)),
+                    IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.heart))
                   ],)
                 ],
               ),
